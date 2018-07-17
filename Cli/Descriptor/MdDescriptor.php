14dfbb59139d7d5ac1fe0e91a186ec04167bb25a
bdm_benzor <benzor@free.fr>
2018-07-18 01:51:13 +0200

    Add new command && output format

--- Cli/Descriptor/MdDescriptor.php
+++ Cli/Descriptor/MdDescriptor.php
@@ -19,28 +19,29 @@ class MdDescriptor extends AbstractDescriptor
 {
     protected function describeAnalysis(Analysis $analysis, array $options = array())
     {
+        $text = [];
         $output = $options['output'];
-        if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $output->getVerbosity()) {
-            $output->write(sprintf('Began at: *%s*', $analysis->getBeginAt()->format('Y-m-d h:i:s')));
-        }
+//        if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $output->getVerbosity()) {
+//            $output->write(sprintf('Began at: *%s*', $analysis->getBeginAt()->format('Y-m-d h:i:s')));
+//        }
         if (!$analysis->isFinished()) {
-            if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $output->getVerbosity()) {
-                $output->writeln('');
-            }
+//            if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $output->getVerbosity()) {
+//                $output->writeln('');
+//            }
             $output->writeln('The analysis is not finished yet.');
 
             return;
         }
-        if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $output->getVerbosity()) {
-            $output->write(sprintf(' Ended at: *%s*', $analysis->getEndAt()->format('Y-m-d h:i:s')));
-            $output->writeln(sprintf(' Real duration: *%s*.', $analysis->getEndAt()->format('Y-m-d h:i:s')));
-            $output->writeln('');
-        }
-        $output->write(sprintf(
+//        if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $output->getVerbosity()) {
+//            $output->write(sprintf(' Ended at: *%s*', $analysis->getEndAt()->format('Y-m-d h:i:s')));
+//            $output->writeln(sprintf(' Real duration: *%s*.', $analysis->getEndAt()->format('Y-m-d h:i:s')));
+//            $output->writeln('');
+//        }
+        $text[] = sprintf(
             'The project has **%d violations**, it got the *%s grade*.   ',
             $analysis->getNbViolations(),
             $analysis->getGrade()
-        ));
+        );
 
         $grades = $analysis->getGrades();
         $bestGrade = end($grades);
@@ -50,14 +51,14 @@ class MdDescriptor extends AbstractDescriptor
             return;
         }
 
-        $output->writeln(sprintf(
+        $text[]= sprintf(
             ' **%d hours** to get the **%s grade** and %d hours to get the %s grade   ',
             $analysis->getRemediationCostForNextGrade(),
             $analysis->getNextGrade(),
             $analysis->getRemediationCost(),
             $bestGrade
-        ));
-        $output->writeln('   ');
+        );
+        $text[]= '   ';
 
         if ($analysis->getViolations()) {
 
@@ -108,39 +109,41 @@ EOL;
                 $ref[$key]['resources'][] = $violation->getResource().':'.$violation->getLine();
             }
 
-            $output->writeln(sprintf(
+            $text[]= sprintf(
                 ' We find **%s new violation** in this pull request.   ',
                 count($analysis->getViolations())
-            ));
-            $output->writeln('   ');
+            );
+            $text[]= '   ';
 
-            $output->writeln("<table>");
+            $text[]= "<table>";
             foreach ($ref as $violation) {
-                $output->writeln(strtr($template, array(
+                $text[]= strtr($template, array(
                     '{{ resources }}'   => implode('</li><li>',$violation['resources']),
                     '{{ category }}'    => $violation['category'],
                     '{{ severity }}'    => $violation['severity'],
                     '{{ title }}'       => $violation['title'],
                     '{{ message }}'     => $violation['message'],
-                )));
+                ));
             }
-            $output->writeln("</table>");
+            $text[]= "</table>";
         } else {
 
 
-            $output->writeln(sprintf(
+            $text[]= sprintf(
                 ' This pull request has no new violation, is great !! <3<3<3   ',
                 count($analysis->getViolations())
-            ));
-            $output->writeln('   ');
+            );
+            $text[]= '   ';
         }
 
         foreach ($analysis->getLinks() as $link) {
             if ('self' == $link->getRel() && 'text/html' == $link->getType()) {
-                $output->writeln(sprintf('You can get the full report [here](%s)', $link->getHref()));
+                $text[]= sprintf('You can get the full report [here](%s)', $link->getHref());
 
                 break;
             }
         }
+
+        $output->writeln(json_encode(['body'=>implode("\n",$text)]));
     }
 }
